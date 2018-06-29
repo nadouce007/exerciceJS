74d23e00bd9dd2186b9146e7a77247b9d9b86e30
nadia <nadia.aissaoui.pro@gmail.com>
2018-06-29 14:19:53 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+/*exercice1*/
+
+/*console.log ("hello word");
+var Bonjour = "Bonjour à tous";
+console.log (Bonjour);*/
+
+/*exercice2*/
+
+/*console.log (12/0);
+console.log (4+9+78);
+console.log (12-7);
+console.log (5e4);*/
+
+/*exercice3*/
+
+/*var Nom = prompt ("quel est votre nom?");
+alert ("Bienvenue"+ Nom);*/
+
+/*exercice4*/
+
+/*var Nom ="Aissaoui";
+var Prénom ="Nadia";
+console.log ("Bienvenue" + Nom + Prénom);*/
+
+/*exercice5*/
+
+/*var Nombre = "123";
+Nombre = Number(Nombre);
+console.log (Nombre +123);*/
+
+/*exercice6*/
+/*var unTexte ="gracias";
+console.log(unTexte.toUpperCase());
+console.log(unTexte.toLowerCase());*/
+
+
+
+
+/*Partie2
+
+exercice1*/
+/*
+var unTexte ="Buongiorno";
+var autreTexte ="";
+if (unTexte.length === 0) {
+  console.log(false);
+} else {
+    console.log(true);
+}
+
+if (autreTexte.length === 0) {
+  console.log(false);
+} else {
+  console.log(true);
+
+}*/
+
+
+
+
+
+
+
+
+/*commentaire*/
